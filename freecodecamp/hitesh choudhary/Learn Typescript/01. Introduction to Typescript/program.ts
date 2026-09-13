58567f527c8fadc911c3ea57180eda40a38ed6bf
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-13 18:06:41 +0530

    Introduction and overview of typescript

---
+++ freecodecamp/hitesh choudhary/Learn Typescript/01. Introduction to Typescript/program.ts
@@ -0,0 +1,20 @@
+//* Introduction to Typescript 
+
+/**
+ * ? What is Typescript ? 
+ *  - Typescript basically the superset of javascript, which means what the features provided in javascript are already present in typescript with type guard. 
+ * - Typescript is the type guard layer over the javascript that's not mean it's provide new programmatic features to javascript just type safety due to dynamic nature of javascript. 
+ * - As we understand that Typescript provide type safety because of javascript odd or strange behavior that's sometimes affect our code logic for example operation of two types like 2 + '2' give '22', 'abc1' + 22 = NaN that's odd 
+ * - Typescript help to give more control over the javascript type which doesn't we do in javascript to write more error-pron and precise code. 
+ * ! - Typescript code will transpile at the last in javascript only so we can catch the runtime only and lot of editor supports some feature to highlight the errors like wiggly lines to represent error.  
+ * 
+ * ? What features Typescript Provide ? 
+ * Typescript have only one responsibility is to check the Static Type Checking same feature which provide by programming language like c++, java, rust etc. 
+ * 
+ * In javascript whatever we code write it's okay if the code is buggy or not but when we execute in their environment "like browser or node" than it give error after that we have to fix so typescript catches that code in runtime environment by providing hints and if we are writing our code in IDEs than it also support extra features of hint like highlighted with red color or squiggly line or wiggly line that's help to catch error and write more secure and error pron code. 
+ * 
+ * Typescript  code at last will transpile into Javascript only because typescript provide only safe guard so we have to write more code in typescript which will be more lesser in javascript after converted from typescript code, 
+ * 
+ * * Typescript is programming language which provide type safety layer over the javascript or rather we can also called it development tool which provide wrapper of type safety over javascript to write more error pron, precise and scalable javascript code.  
+ */
+
